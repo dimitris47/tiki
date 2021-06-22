b5e61593831e9a44bbeb6b258bb72321fefaefd4
dimitris47 <dimitrisinbox@gmail.com>
2021-06-22 03:17:26 +0300

    simplify code

--- project.cpp
+++ project.cpp
@@ -7,15 +7,10 @@ Project::Project(QString name) {
 
 void Project::prioritySort() {
     QList<Task> newList;
-    for (int i = 0; i <= 2; i++) {
-        for (auto &&task : this->tasks)
-            if (task.priority() == i && task.status() == 0)
-                newList.append(task);
-    }
-    for (int i = 0; i <= 2; i++) {
-        for (auto &&task : this->tasks)
-            if (task.priority() == i && task.status() == 1)
-                newList.append(task);
-    }
+    for (int st = 0; st <=1; st++)
+        for (int i = 0; i <= 2; i++)
+            for (auto &&task : this->tasks)
+                if (task.priority() == i && task.status() == st)
+                    newList.append(task);
     tasks = newList;
 }
