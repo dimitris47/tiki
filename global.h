de4eb3d2be9ae599419e55a44b3c4fd41897f8eb
dimitris47 <dimitrisinbox@gmail.com>
2021-06-29 11:04:21 +0300

    add license to sources & icon attributions

--- global.h
+++ global.h
@@ -10,7 +10,9 @@ const QString CONTRIBUTORS("Dimitris Psathas");
 const QString APPINFO("Program created by Dimitris Psathas" + br.repeated(2) +
                       "Written in C++, built against Qt 5.15.2" + br.repeated(2) +
                       "Published under the GNU General Public License v3.0" + br +
-                      "Qt libraries used under (L)GPL3" + br.repeated(2) +
+                      "Qt libraries used under (L)GPL3" + br +
+                      "Application icon by <a href='https://icons8.com'>icons8.com</a>" + br +
+                      "In-app icons by the KDE Project" + br.repeated(2) +
                       "&copy; 2021 Dimitris Psathas");
 
 #endif // GLOBAL_H
