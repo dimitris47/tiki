00acf8267aab81222b006001c4cc8caa9fa1b090
dimitris47 <dimitrisinbox@gmail.com>
2021-09-24 11:29:43 +0300

    add icon

--- global.h
+++ global.h
@@ -8,7 +8,7 @@ const QString APPNAME("Tiki");
 const QString VERSION("2.8");
 const QString CONTRIBUTORS("Dimitris Psathas");
 const QString APPINFO("Program created by Dimitris Psathas" + br.repeated(2) +
-                      "Written in C++, built against Qt 5.15.2" + br.repeated(2) +
+                      "Written in C++, built against Qt" + br.repeated(2) +
                       "Published under the GNU General Public License v3.0" + br +
                       "Qt libraries used under (L)GPL3" + br +
                       "Application icon by <a href='https://icons8.com'>icons8.com</a>" + br +
